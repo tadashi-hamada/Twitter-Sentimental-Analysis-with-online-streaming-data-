fa308dcca9f4de95ce5f53f4bf2c3c8e43f89d81
gentlecoder <35189816+tadashi-hamada@users.noreply.github.com>
2020-09-20 17:42:09 +0530

    updated

---
+++ tr4.py
@@ -0,0 +1,181 @@
+from tweepy import API 
+from tweepy import Cursor
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import tweepy
+import numpy as np
+import pandas as pd
+import re
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import *
+from nltk.classify import NaiveBayesClassifier
+#from wordcloud import WordCloud
+import matplotlib.pyplot as plt 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, confusion_matrix, accuracy_score
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from textblob.np_extractors import ConllExtractor
+nltk.download('stopwords')
+nltk.download('wordnet')
+stopwords = set(stopwords.words("english"))
+
+tweets = []
+class TwitterClient(object): 
+    def __init__(self): 
+        #Initialization method. 
+        ACCESS_TOKEN = '1145429812145577985-huYYValfVedAHCJjrERho1kYIFcGEv'
+        ACCESS_SECRET = 'kJtQzUwq9tYgDVI1J4faqT8eSd8Pd3F2BNWY0OiJD8106'
+        CONSUMER_KEY = 'YAXmxyUuD0S9XWjqpIED8lMTt'
+        CONSUMER_SECRET = '7SOMXc1VzE3Xcq4ib9B1CZmOwWRxKWJtiqTHi1xWZ9NLomM3qS'
+        try: 
+            # create OAuthHandler object 
+            auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+            auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+            # create tweepy API object to fetch tweets 
+            self.api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+            
+        except tweepy.TweepError as e:
+            print(f"Error: Tweeter Authentication Failed - \n{str(e)}")
+
+    def get_tweets(self, query, maxTweets = 1000):
+        #Function to fetch tweets. 
+        # empty list to store parsed tweets 
+        #tweets = [] 
+        sinceId = None
+        max_id = -1
+        tweetCount = 0
+        tweetsPerQry = 100
+
+        while tweetCount < maxTweets:
+            try:
+                if (max_id <= 0):
+                    if (not sinceId):
+                        new_tweets = self.api.search(q=query, count=tweetsPerQry)
+                    else:
+                        new_tweets = self.api.search(q=query, count=tweetsPerQry,
+                                                since_id=sinceId)
+                else:
+                    if (not sinceId):
+                        new_tweets = self.api.search(q=query, count=tweetsPerQry,
+                                                max_id=str(max_id - 1))
+                    else:
+                        new_tweets = self.api.search(q=query, count=tweetsPerQry,
+                                                max_id=str(max_id - 1),
+                                                since_id=sinceId)
+                if not new_tweets:
+                    print("No more tweets found")
+                    break
+
+                for tweet in new_tweets:
+                    parsed_tweet = {} 
+                    parsed_tweet['tweets'] = tweet.text 
+
+                    # appending parsed tweet to tweets list 
+                    if tweet.retweet_count > 0: 
+                        # if tweet has retweets, ensure that it is appended only once 
+                        if parsed_tweet not in tweets: 
+                            tweets.append(parsed_tweet) 
+                    else: 
+                        tweets.append(parsed_tweet) 
+                        
+                tweetCount += len(new_tweets)
+                print("Downloaded {0} tweets".format(tweetCount))
+                max_id = new_tweets[-1].id
+
+            except tweepy.TweepError as e:
+                # Just exit if any error
+                print("Tweepy error : " + str(e))
+                break
+        
+        return pd.DataFrame(tweets)
+
+    def fetch_sentiment_using_textblob(self,tweet):
+        analysis = TextBlob(tweet)
+        
+        if analysis.sentiment.polarity > 0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
+
+    def remove_pattern(self,text, pattern_regex):
+        r = re.findall(pattern_regex, text)
+        for i in r:
+            text = re.sub(i, '', text)
+    
+        return text     
+
+twitter_client = TwitterClient()
+searchTerm = input("Enter Keyword/Tag to search about: ")
+NoOfTerms = int(input("Enter how many tweets to search: "))
+tweets_df = twitter_client.get_tweets('searchTerm', maxTweets=NoOfTerms)
+#tw= API.search(q='searchTerm', count=NoOfTerms)
+
+#print(f'tweets_df Shape - {tweets_df.shape}')
+print(tweets_df.head(10))        
+sentiments_using_textblob = tweets_df.tweets.apply(lambda tweet: twitter_client.fetch_sentiment_using_textblob(tweet))
+tweets_df['sentiment'] = sentiments_using_textblob
+#print(tweets_df.head())
+#print(pd.DataFrame(sentiments_using_textblob.value_counts()))
+tweets_df['tidy_tweets'] = np.vectorize(twitter_client.remove_pattern)(tweets_df['tweets'], "@[\w]*: | *RT*")
+print(tweets_df['sentiment'].head(10))
+
+
+cleaned_tweets = []
+
+for index, row in tweets_df.iterrows():
+    # Here we are filtering out all the words that contains link
+    words_without_links = [word for word in row.tidy_tweets.split() if 'http' not in word]
+    cleaned_tweets.append(' '.join(words_without_links))
+
+tweets_df['tidy_tweets'] = cleaned_tweets
+#print(tweets_df.head(10))
+
+tweets_df = tweets_df[tweets_df['tidy_tweets']!='']
+#tweets_df.head()
+
+tweets_df.drop_duplicates(subset=['tidy_tweets'], keep=False)
+tweets_df = tweets_df.reset_index(drop=True)
+tweets_df['absolute_tidy_tweets'] = tweets_df['tidy_tweets'].str.replace("[^a-zA-Z# ]", "")
+
+stopwords_set = set(stopwords)
+cleaned_tweets = []
+
+for index, row in tweets_df.iterrows():
+    
+    # filerting out all the stopwords 
+    words_without_stopwords = [word for word in row.absolute_tidy_tweets.split() if not word in stopwords_set and '#' not in word.lower()]
+    
+    # finally creating tweets list of tuples containing stopwords(list) and sentimentType 
+    cleaned_tweets.append(' '.join(words_without_stopwords))
+    
+tweets_df['absolute_tidy_tweets'] = cleaned_tweets
+#print(tweets_df.head(10))
+#print(tweets_df.head())
+
+tokenized_tweet = tweets_df['absolute_tidy_tweets'].apply(lambda x: x.split())
+tokenized_tweet.head()
+word_lemmatizer = WordNetLemmatizer()
+
+tokenized_tweet = tokenized_tweet.apply(lambda x: [word_lemmatizer.lemmatize(i) for i in x])
+tokenized_tweet.head()
+for i, tokens in enumerate(tokenized_tweet):
+    tokenized_tweet[i] = ' '.join(tokens)
+
+tweets_df['absolute_tidy_tweets'] = tokenized_tweet
+print(tweets_df.head())
+
+
+
+
